c80711c2d72d5aa1bc0e10ba214c5fd397abd3e2
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-06-29 09:17:10 -0600

    script.js
    Javascript file

---
+++ script.js
@@ -0,0 +1,8 @@
+var player = $('#player');
+$(document).keydown(function(e) {
+	if (e.keyCode === 68 && player.position().left < 215) {
+		player.css("left", player.position().left + 5 + 'px');
+	} else if (e.keyCode === 65 && player.position().left > -215) {
+		player.css("left", player.position().left - 5 + 'px');
+	}
+});
