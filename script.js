6c888d30de9edc3fe4654d8a0520af2ed9d5bfdb
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-06-29 18:57:11 -0600

    Undo
    Undid my previous change since it bugged up in JSFiddle

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var player = $('#player');
 var cpu = $("#cpu");
 var ball = $("#ball");
-var ballDir = round(Math.random() * (365 - 1) + 1);
 $(document).keydown(function(e) {
 	if (e.keyCode === 68 && player.position().left < 215) {
 		player.css("left", player.position().left + 5 + 'px');
