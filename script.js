32d9efaca4219d34c24672b8dcbd548ccbb630cd
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-06-29 18:52:48 -0600

    Update script.js
    Added a random ball variable, and initialized it.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var player = $('#player');
 var cpu = $("#cpu");
 var ball = $("#ball");
+var ballDir = round(Math.random() * (365 - 1) + 1);
 $(document).keydown(function(e) {
 	if (e.keyCode === 68 && player.position().left < 215) {
 		player.css("left", player.position().left + 5 + 'px');
