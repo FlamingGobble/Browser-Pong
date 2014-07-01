3dbb7f315e34cf124e0585181e0bf46ad8953e0b
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-06-30 18:44:56 -0600

    Update script.js
    Uber-basic AI.

--- script.js
+++ script.js
@@ -15,19 +15,29 @@ $(document).mouseenter(function () {
     if (starting) {
         $("#ball").click(function () {
             starting = false;
+//BALL MOTION
             setInterval(function () {
                 ball.css("top", ball.position().top + (dist * Math.sin(ballDir)) + 'px');
                 ball.css("left", ball.position().left + (dist * Math.cos(ballDir)) + 'px');
                 if (ball.position().left < 0 || ball.position().left > 500) {
-                    ball.css("left", ball.position().left + Math.sin(ballDir)*(0-dist));
+                    ball.css("left", (ball.position().left + Math.sin(ballDir)*(0-dist)) + "px");
                     if (ball.position().left < 0 || ball.position().left > 500) {
                         ballDir = (180-((180/Math.PI)*ballDir))*(Math.PI/180);
                     } else {
                         ballDir = (0-((180/Math.PI)*ballDir))*(Math.PI/180);
                     }
-                    ball.css("left", ball.position().left + Math.sin(ballDir)*dist);
+                    ball.css("left", (ball.position().left + Math.sin(ballDir)*dist) + "px");
                 }
             }, 5);
+//AI MOTION            
+            setInterval(function() {
+                if (ball.position().left > cpu.position().left) {
+                    cpu.css("left", (cpu.position().left + 2) + "px");
+                } else if (ball.position().left < cpu.position().left) {
+                    cpu.css("left", (cpu.position().left - 2) + "px");
+                }
+            }, 5);
+            
         });
     }
 });
