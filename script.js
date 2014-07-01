a657cce15269800597f2768e41cea2776bda9525
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-07-01 09:39:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var ball = $("#ball");
 var starting = true;
 var dist = 2;
 var playerSpeed = 10;
+var cpuSpeed = 2;
 $(document).keydown(function (e) {
     if (e.keyCode === 68 && player.position().left < 215) {
         player.css("left", player.position().left + playerSpeed + 'px');
@@ -16,24 +17,30 @@ $(document).mouseenter(function () {
     $("#ball").click(function () {
         if (starting) {
             setInterval(function () {
+                //Function for the ball's bounce
+                function ballBounce() {
+                    if (ball.position().left < 0 || ball.position().left > 500) {
+                        ball.css("left", (ball.position().left + Math.sin(ballDir) * (0 - dist)) + "px");
+
+                        if (ball.position().left < 0 || ball.position().left > 500) {
+                            ballDir = (180 - ((180 / Math.PI) * ballDir)) * (Math.PI / 180);
+                        } else {
+                            ballDir = (0 - ((180 / Math.PI) * ballDir)) * (Math.PI / 180);
+                        }
+                        ball.css("left", (ball.position().left + Math.sin(ballDir) * dist) + "px");
+                    }
+                }
+                //Ball actual motion
                 ball.css("top", ball.position().top + (dist * Math.sin(ballDir)) + 'px');
                 ball.css("left", ball.position().left + (dist * Math.cos(ballDir)) + 'px');
-                if (ball.position().left < 0 || ball.position().left > 500) {
-                    ball.css("left", (ball.position().left + Math.sin(ballDir) * (0 - dist)) + "px");
-                    if (ball.position().left < 0 || ball.position().left > 500) {
-                        ballDir = (180 - ((180 / Math.PI) * ballDir)) * (Math.PI / 180);
-                    } else {
-                        ballDir = (0 - ((180 / Math.PI) * ballDir)) * (Math.PI / 180);
-                    }
-                    ball.css("left", (ball.position().left + Math.sin(ballDir) * dist) + "px");
-                }
+                ballBounce();
             }, 5);
             //AI MOTION            
             setInterval(function () {
                 if (ball.position().left > cpu.position().left + 30) {
-                    cpu.css("left", (cpu.position().left + 2) + "px");
+                    cpu.css("left", (cpu.position().left + cpuSpeed) + "px");
                 } else if (ball.position().left < cpu.position().left + 30) {
-                    cpu.css("left", (cpu.position().left - 2) + "px");
+                    cpu.css("left", (cpu.position().left - cpuSpeed) + "px");
                 }
             }, 5);
         }
